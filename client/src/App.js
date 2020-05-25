7d9eb599bfb7e235aaf36dafa360b0d0ba2e15e2
Prashanth <yvprashanth@gmail.com>
2020-05-25 14:52:04 -0700

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import LevelsForm from './components/Levelsform';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import Maindata from './components/Alldata';
 
 class App extends React.Component {
 
@@ -21,13 +28,24 @@ class App extends React.Component {
 
   render(){
     return (
+      <Router>
+
       <div className="App">
         <Navbar className="Color-nav" light expand="md">
           <NavbarBrand className="mx-auto" href="/" style={{color:'white'}}>India Levels.fyi</NavbarBrand>
         </Navbar>
         <br/>
-        <LevelsForm />
+        
+        <Switch>
+          <Route path="/maindata">
+            <Maindata />
+          </Route>
+          <Route path="/">
+            <LevelsForm />
+          </Route>
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
