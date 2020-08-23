93e4d7b7eae3aee4743995cf6b0651ac26054b47
prashanthy <tomu644@gmail.com>
2020-08-22 21:21:28 -0700

    Minor changes to get the UI Form in shape

--- client/src/components/Alldata.js
+++ client/src/components/Alldata.js
@@ -9,24 +9,33 @@ class Alldata extends React.Component {
         super(props);
         this.state = {
           columnDefs: [
-            { headerName: "Make", field: "make" },
-            { headerName: "Model", field: "model" },
-            { headerName: "Price", field: "price" }],
-          rowData: [
-            { make: "Toyota", model: "Celica", price: 35000 },
-            { make: "Ford", model: "Mondeo", price: 32000 },
-            { make: "Porsche", model: "Boxter", price: 72000 }]
-        }
+            { headerName: "Company", field: "company", checkboxSelection: true, sortable: true, filter: true },
+            { headerName: "Level", field: "level", sortable: true},
+            { headerName: "Total Compensation", field: "totalyearlycompensation", sortable: true},
+            { headerName: "Location", field: "location", sortable: true }]
+        };
     }
     
+    componentDidMount(){
+        fetch("https://kpn3r0hyj6.execute-api.us-west-2.amazonaws.com/prod", {
+            method:"GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              }
+        }).then(response => {
+            response.json().then(data => {
+                console.log(data);
+                this.setState({rowData: data.body.Items});
+            });
+        });
+    }
+
     render(){
         return (
             <Container>
                      <div
-                        className="ag-theme-alpine"
-                        style={{
-                        height: '250px',
-                        width: '600px' }} >
+                        className="ag-theme-alpine"  style={ {height: '40em', width: '80em'} }>
                         <AgGridReact
                         columnDefs={this.state.columnDefs}
                         rowData={this.state.rowData}>
--- client/src/components/Levelsform.js
+++ client/src/components/Levelsform.js
@@ -174,7 +174,6 @@ class LevelsForm extends React.Component{
     }
 
     render() {
-        
             return (
                 <Container>
                     <Row>
